cb6614da388343507b8ca1f60030efa758ccdfce
Paraschiv Alexandru Cosmin <alexandru.paraschiv2507@gmail.com>
2020-03-13 16:13:49 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,99 @@
+# Configure the Azure Provider
+provider "azurerm" {
+  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
+  version = "=2.0.0"
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-resources"
+  location = var.location
+  tags = {
+    environment = "Development"
+  }
+}
+
+resource "azurerm_virtual_network" "vn" {
+  name                = "${var.prefix}-network"
+  address_space       = var.virtual_network_address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags = {
+    environment = "Development"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "${var.prefix}-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vn.name
+  address_prefix       = var.subnet_address_prefix
+}
+
+resource "azurerm_public_ip" "pip" {
+  name                    = "${var.prefix}-test-pip"
+  location                = azurerm_resource_group.rg.location
+  resource_group_name     = azurerm_resource_group.rg.name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+  tags = {
+    environment = "Development"
+  }
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.prefix}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "${var.prefix}-conf"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = var.private_ip_address_allocation
+    public_ip_address_id          = azurerm_public_ip.pip.id
+  }
+  tags = {
+    environment = "Development"
+  }
+}
+
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "${var.prefix}-vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = var.vm_size
+
+  # Uncomment this line to delete the OS disk automatically when deleting the VM
+  # delete_os_disk_on_termination = true
+
+  # Uncomment this line to delete the data disks automatically when deleting the VM
+  # delete_data_disks_on_termination = true
+
+  storage_image_reference {
+    publisher = var.os_image_publisher
+    offer     = var.os_image_offer
+    sku       = var.os_image_sku
+    version   = var.os_image_version
+  }
+  storage_os_disk {
+    name              = "${var.prefix}-myosdisk1"
+    caching           = var.os_disk_caching
+    create_option     = var.os_disk_create_option
+    managed_disk_type = var.os_disk_managed_disk_type
+  }
+  os_profile {
+    computer_name  = "${var.prefix}hostname"
+    admin_username = var.os_admin_username
+  }
+  os_profile_linux_config {
+    disable_password_authentication = var.disable_password_authentication
+    ssh_keys {
+      path     = "/home/${var.os_admin_username}/.ssh/authorized_keys"
+      key_data = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQChe0xfuNe7yJUeB64MBsLjShb7mhdSo0uUwAUNbvxoKu1yZXBeCZLec9uHD1jJW0RpVniB0pz/fVIeCSKsbdMOAsWHDtV/bv7TTuzYVnEbN4KrAT3WykTMdzAJEwspO5h41r6mKhn2xzkuPiJRqqmP6odDgYXbzo8gpSqZbohIq+8dhIZ5FBYwjcNU8EBZYKaCLwcsp5njF8vwzwTJr6A0fPMrynuZErEw4kqsxHBeaxRIGVHecqwci6agkG+64jFrMUOVDrwMYrR24TgeuAE6ycnQ8bU60YlHbJfWe/95PzF53Z/3fDC/sBX4mVmtAEQI2CFty7v8hey8+7v9Tylj aparaschiv@ROAPARASCHIV03M"
+    }
+  } 
+  tags = {
+    environment = "Development"
+  }
+}
