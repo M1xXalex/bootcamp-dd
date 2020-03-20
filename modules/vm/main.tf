943e541a0d125c84e6684c067421939af5036150
Paraschiv Alexandru Cosmin <alexandru.paraschiv2507@gmail.com>
2020-03-20 13:01:01 +0200

    removed duplicate code

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,19 +1,3 @@
-# Configure the Azure Provider
-provider "azurerm" {
-  # whilst the `version` attribute is optional, we recommend pinning to a given version of the Provider
-  version = "=2.1.0"
-  features {}
-}
-
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "bootcamp-terraform"
-    storage_account_name = "bootcamptfstatestg"
-    container_name       = "tfstate"
-    key                  = "bootcampdev.tfstate"
-  }
-}
-
 resource "azurerm_resource_group" "rg" {
   name     = "${var.prefix}-resources"
   location = var.location
